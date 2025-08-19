d0abd9f6b50d8e005e37a99fc0850099d9021412
Chetan Agarwal <agarwalchetan1302@gmail.com>
2025-08-19 22:38:07 +0530

    Start Java 1

--- src/com/agarwal/Main.java
+++ src/com/agarwal/Main.java
@@ -2,14 +2,11 @@ package com.agarwal;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 15;
-        int b = 20;
-
+        int a = 2;
+        int b = 0;
         int c = a + 2*(b+a);
-
         int count = 1;
         for(int i = 0; i <= c ; i++){
-
             System.out.println(count + " Hello World");
             count++;
         }
