b22539e048ea6760f326861991ee81979ae7f56d
Chetan Agarwal <agarwalchetan1302@gmail.com>
2025-09-12 00:22:41 +0530

    Arrays Completed

---
+++ src/com/arrays/ArrayList1.java
@@ -0,0 +1,15 @@
+package com.arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayList1 {
+    public static void main(String[] args) {
+        //Array
+        int[] arr = new int[5];
+        System.out.println(Arrays.toString(arr));
+
+        //ArrayList
+        ArrayList<Integer> list = new ArrayList<>();
+    }
+}
